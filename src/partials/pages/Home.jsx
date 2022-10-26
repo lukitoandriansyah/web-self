63c525ad510ca5066ca410f4d58b269159fb2973
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-10-26 20:28:42 +0700

    revise

--- src/partials/pages/Home.jsx
+++ src/partials/pages/Home.jsx
@@ -47,12 +47,10 @@ export default function Home() {
                                 </ol>
                                 <div className={"row"}>
                                     <div className={"card bg-info text-white"}>
-                                        <div className={"card-body bg-primary mt-4 mb-4 text-center"}>
-                                            <i className="fa fa-info fa-2x" aria-hidden="true"/>
-                                            <br/><br/>
-                                            <h6>This web still in progress to build, some feature might be can't running perfectly.</h6>
-                                            <h6>sorry for our distraction<br/><br/></h6>
-                                        </div>
+                                        <i className="fa fa-info fa-2x" aria-hidden="true"/>
+                                        <br/><br/>
+                                        <h6>This web still in progress to build, some feature might be can't running perfectly.</h6>
+                                        <h6>sorry for our distraction<br/><br/></h6>
                                     </div>
                                 </div>
                                 <br/>
