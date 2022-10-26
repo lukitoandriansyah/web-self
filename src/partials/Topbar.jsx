dc4227bb536d4d666584069d501cb5c1c32a2824
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-10-26 18:34:32 +0700

    revise

--- src/partials/Topbar.jsx
+++ src/partials/Topbar.jsx
@@ -3,6 +3,22 @@ export default function Topbar(){
     window.addEventListener('DOMContentLoaded', event => {
 
         // Toggle the side navigation
+        /*const sidebarToggle = document.body.querySelector('#sidebarToggle');
+        if (sidebarToggle) {
+            // Uncomment Below to persist sidebar toggle between refreshes
+            // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+            //     document.body.classList.toggle('sb-sidenav-toggled');
+            // }
+            sidebarToggle.addEventListener('click', event => {
+                event.preventDefault();
+                document.body.classList.toggle('sb-sidenav-toggled');
+                localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            });
+        }*/
+
+    });
+
+    function toggleSet(){
         const sidebarToggle = document.body.querySelector('#sidebarToggle');
         if (sidebarToggle) {
             // Uncomment Below to persist sidebar toggle between refreshes
@@ -15,12 +31,11 @@ export default function Topbar(){
                 localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
             });
         }
-
-    });
+    }
     return<>
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a className="navbar-brand ps-3" href="#">Lukito Andriansyah</a>
-            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#"><i
+            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#" onDOMContentLoaded = {(event) => {toggleSet(event)}}><i
                 className="fas fa-bars"></i></button>
             {/*<form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div className="input-group">
